89c43f2353a58ba6d323d5048c2ad346ba761423
Ryan McCombs <mccombs008@yahoo.com>
2019-01-23 19:25:56 -0700

    connected routes and anchors on Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,25 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer(props) {
-  return(
-      <div className="footerContainer">
-        <ul>
-            <li>About</li>
-            <li>•</li>
-            <li>Settings</li>
-            <li>•</li>
-            <li>Help</li>
-            <li>•</li>
-            <li>API</li>
-            <li>•</li>
-            <li>Hacker News</li>
-            <li>•</li>
-            <li>Fork/Contribute</li>
-            <li>•</li>
-            <li>Status</li>
-            <li>•</li>
-            <li>Cool apps</li>
-        </ul>
-      </div>
+  return (
+    <div className="footerContainer">
+      <ul>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <li>•</li>
+        <Link to="/settings">
+          <li>Settings</li>
+        </Link>
+        <li>•</li>
+        <Link to="/help">
+          <li>Help</li>
+        </Link>
+        <li>•</li>
+        <Link to="/api">
+          <li>API</li>
+        </Link>
+        <li>•</li>
+
+        <li>
+          <Link to="hackerNews">Hacker News</Link>
+        </li>
+        <li>•</li>
+        <li>
+          <a href="https://github.com/algolia/hn-search">Fork/Contribute</a>
+        </li>
+        <li>•</li>
+        <li>Status</li>
+        <li>•</li>
+        <li>
+          <Link to="coolApps">Cool apps</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
