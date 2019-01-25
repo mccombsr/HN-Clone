b1a11a0704689c4969107ae383c59515a91eae50
Ryan McCombs <mccombs008@yahoo.com>
2019-01-25 16:44:48 -0700

    trying to get settings labels and selectors lined up correctly

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -13,55 +13,91 @@ export default class Settings extends Component {
         <BlankHeader />
         <div className="settingsContainer">
           <h1 className="settingsTitle">Settings</h1>
-          <div className="display">
-            <h2>Display options</h2>
-            <div className="displayOptions">
-              <p>Style</p>
+          <div className="settings">
+            {/* Display Settings */}
+            <h2 className="section">Display options</h2>
+            <hr className="section" />
+            <div className="display">
+              {/* Select dom style */}
+              <div className="group">
+                <p>Style</p>
+                <select>
+                  <option value="default">Default</option>
+                  <option value="experimental">Experimental</option>
+                </select>
+              </div>
+
+              {/* select hits per page */}
+              <div className="group">
+                <p>Hits per page</p>
+                <select>
+                  <option value="" />
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="30">30</option>
+                  <option value="50">50</option>
+                </select>
+              </div>
+            </div>
+
+            {/* Ranking settings */}
+            <h2 className="section">Ranking options</h2>
+            <hr className="section" />
+
+            {/* Select default search type */}
+            <div className="selector">
+              <p>Default type</p>
               <select>
-                <option value="default">Default</option>
-                <option value="experimental">Experimental</option>
-              </select>
-              <p>Hits per page</p>
-              <select>
-                <option value="" />
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="30">30</option>
-                <option value="50">50</option>
+                <option value="all">All</option>
+                <option value="stories" selected>
+                  Stories
+                </option>
+                <option value="comments">Comments</option>
               </select>
             </div>
-            <h2>Ranking options</h2>
-            <p>Default type</p>
-            <select>
-              <option value="all">All</option>
-              <option value="stories" selected>
-                Stories
-              </option>
-              <option value="comments">Comments</option>
-            </select>
-            <p>Default sort order</p>
-            <select>
-              <option value="recent">Most Recent First</option>
-              <option value="popular" selected>
-                Most Popular First
-              </option>
-            </select>
-            <p>Default date range</p>
-            <select>
-              <option value="24">Last 24h</option>
-              <option value="week">Past Week</option>
-              <option value="month">Past Month</option>
-              <option value="year">Past Year</option>
-              <option value="forever" selected>
-                Forever
-              </option>
-            </select>
-            <p>Use the story text for the search</p>
-            <input type="checkbox" />
-            <p>Use the author's username for the search</p>
-            <input type="checkbox" />
-            <p>Typo-tolerance</p>
-            <input type="checkbox" />
+
+            {/* Select default sort order */}
+            <div className="selector">
+              <p>Default sort order</p>
+              <select>
+                <option value="recent">Most Recent First</option>
+                <option value="popular" selected>
+                  Most Popular First
+                </option>
+              </select>
+            </div>
+
+            {/* Select default date range */}
+            <div className="selector">
+              <p>Default date range</p>
+              <select>
+                <option value="24">Last 24h</option>
+                <option value="week">Past Week</option>
+                <option value="month">Past Month</option>
+                <option value="year">Past Year</option>
+                <option value="forever" selected>
+                  Forever
+                </option>
+              </select>
+            </div>
+
+            {/* Toggle story text search parameter */}
+            <div className="selector">
+              <p>Use the story text for the search</p>
+              <input type="checkbox" />
+            </div>
+
+            {/* Toggle author's name search parameter */}
+            <div className="selector">
+              <p>Use the author's username for the search</p>
+              <input type="checkbox" />
+            </div>
+
+            {/* Toggle typo-tolerance in search */}
+            <div className="selector">
+              <p>Typo-tolerance</p>
+              <input type="checkbox" />
+            </div>
           </div>
         </div>
       </div>
